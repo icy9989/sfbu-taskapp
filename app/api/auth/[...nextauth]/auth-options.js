d9aa45402089b390db95c258e69d8c4f234b6b37
Khaing Min Htwe <kmhtwe1999@gmail.com>
2025-04-01 23:20:38 -0700

    add login api

---
+++ app/api/auth/[...nextauth]/auth-options.js
@@ -0,0 +1,67 @@
+import { compare } from "bcrypt";
+import { NextAuthOptions } from "next-auth"
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prismadb from '@/lib/prismadb';
+
+export const authOptions = {
+    providers: [
+        Credentials({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                // username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                if(!credentials?.email || !credentials?.password) {
+                    throw new Error("Credentials are required!");
+                }
+    
+                const user = await prismadb.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+    
+                if(!user || !user.hashedPassword) {
+                    throw new Error("Username does not existed!")
+                }
+    
+                const isCorrectPassword = await compare(credentials.password, user.hashedPassword)
+    
+                if(!isCorrectPassword) {
+                    throw new Error("Password is not correct!");
+                }
+    
+                return user;
+            }
+        })
+    ],
+    callbacks: {
+        jwt({ token, account, user }) {
+          if (account) {
+            token.accessToken = account.access_token
+            token.id = user?.id
+          }
+          return token
+        },
+        session({ session, token }) {
+            session.user.id = token.id;
+            
+            return session;
+          },
+    },
+    pages: {
+        signIn: '/login',
+    },
+    adapter: PrismaAdapter(prismadb),
+    debug: process.env.NODE_ENV === 'development',
+    session: { strategy: 'jwt' },
+    jwt: {
+        secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+    secret: process.env.NEXTAUTH_SECRET
+}
