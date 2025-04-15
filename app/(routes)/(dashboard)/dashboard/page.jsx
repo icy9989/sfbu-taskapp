2618d54d00b7f2b782f7f7f6a8303c1a326f5298
Khaing Min Htwe <kmhtwe1999@gmail.com>
2025-04-15 16:26:14 -0700

    first check frontend

---
+++ app/(routes)/(dashboard)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const DashboardPage = () => {
+  
+    return (
+    <div className="h-full p-5 bg-slate-50/10">
+        testing
+    </div>
+  )
+}
+
+export default DashboardPage
